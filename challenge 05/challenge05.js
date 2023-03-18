5becb185b0d1e0fa7c8bfaa04d71ba84b1a9bc2e
Matheus <mathdk95@gmail.com>
2023-03-18 15:50:36 -0300

    improved challenge

--- challenge 05/challenge05.js
+++ challenge 05/challenge05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-var arrayQualquer = [1, 2, 3, 4, 5];
+var arrayQualquer = [1, 'Matheus', null, { a: 1}, function(){}];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
@@ -14,8 +14,8 @@ function myFunction(arg) {
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-myFunction(arrayQualquer[2]);
-console.log (myFunction(arrayQualquer[2]));
+myFunction(arrayQualquer)[1];
+console.log(myFunction(arrayQualquer)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -24,20 +24,24 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 function pegarValor(arr, num) {
-    return arg[num];    
+    return arr[num];    
 };
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-var arrayDe5Valores = [1, 2, 3 , 4 , 5];
+var arrayDe5Valores = ['Matheus', 2.2, true , [1, 2, 3, 'Matheus'] ,{propriedade: 1, proriedade2: 'Matheus'}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-myFunction(arrayDe5Valores);
-console.log(myFunction(arrayDe5Valores));
+console.log(pegarValor( arrayDe5Valores, 0 ));
+console.log(pegarValor( arrayDe5Valores, 1 ));
+console.log(pegarValor( arrayDe5Valores, 2 ));
+console.log(pegarValor( arrayDe5Valores, 3 ));
+console.log(pegarValor( arrayDe5Valores, 4 ));
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -54,25 +58,24 @@ propriedades:
 os livros.
 */
 function book(nomeLivro) {
-    var objeto = {
-        aprendendo: {
-            quantidadePaginas: 90,
+    var todosLivros = {
+        'Segredos do Ninja': {
+            quantidadePaginas: 986,
             autor: 'Matheus',
             editora: 'VsCode'
         },
-        estudando: {
-            quantidadePaginas: 92,
-            autor: 'Matheus Dias',
+        'Introdução ao HTML5': {
+            quantidadePaginas: 353,
+            autor: 'Matheus & Sei lá',
             editora: 'Braba'
         },
-        logica: {
-            quantidadePaginas: 87,
+        'Smashing CSS': {
+            quantidadePaginas: 347,
             autor: 'Matheus Dark',
             editora: 'Brabíssima'
         }
     };
-    if (nomeLivro === undefined) return objeto;
-    return nomeLivro.objeto;
+    return !nomeLivro ? todosLivros : todosLivros[ nomeLivro ];
 };
 
 /*
@@ -86,18 +89,18 @@ Ainda com a função acima, imprima a quantidade de páginas de um livro qualque
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-console.log('O livro ' + book().logica + ' tem ' + book().logica.quantidadePaginas + ' páginas!');
+console.log('O livro Introdução ao HTML5 tem ' + book( 'Introdução ao HTML5' ).quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-console.log('O autor do livro ' + book().estudando + ' ' + book().estudando.autor);
+console.log('O autor do livro Smashing CSS é ' + book( 'Smashing CSS').autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-console.log('O livro ' + book().aprendendo + 'foi publicado pela editora ' + book().aprendendo.editora + '.');
+console.log('O livro Segredos do Ninja foi publicado pela editora ' + book( 'Segredos do Ninja' ).editora + '.');
